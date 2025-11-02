535b09a3a16c1351bcc02192f21ee1ce62fec4ce
Sanjai Surya V V <sanjaisuryavelumani@gmail.com>
2025-11-02 15:07:06 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,14 @@
 import psycopg2
+import psycopg2.extras
 import os
 
 def get_db_connection():
-    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://epl_db_user:F3WChu5Rbbre8RVPzrldMe42Qm3hSCPw@dpg-d43gpkemcj7s73b2o8b0-a/epl_db")
-    conn = psycopg2.connect(DATABASE_URL)
+    DATABASE_URL = os.getenv(
+        "DATABASE_URL",
+        "postgresql://epl_db_user:F3WChu5Rbbre8RVPzrldMe42Qm3hSCPw@dpg-d43gpkemcj7s73b2o8b0-a.oregon-postgres.render.com/epl_db?sslmode=require"
+    )
+
+    # Use RealDictCursor so query results are dictionaries, not tuples
+    conn = psycopg2.connect(DATABASE_URL, cursor_factory=psycopg2.extras.RealDictCursor)
     conn.autocommit = True
     return conn
